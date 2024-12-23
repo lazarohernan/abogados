fdf2933b28ffa0fc4760e6130cb481118449e7a6
lazarohernan <hlazaroe@gmail.com>
2024-12-23 07:52:28 -0600

    stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -6,6 +6,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2024-12-18.acacia', // Ajusta esto según la versión que realmente necesitas
   typescript: true,
 });
